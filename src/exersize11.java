0ac92312cb4fc6023b7c56bf518fa736ff6a46e9
Dwalker1000 <25walkerd@gmail.com>
2020-09-15 10:02:32 -0500

    fixinig

---
+++ src/exersize11.java
@@ -0,0 +1,25 @@
+public class exersize11 {
+    public static void main(String[] args) {
+        star();
+        System.out.println();
+        star();
+        line();
+        star();
+    }
+    public static void star(){
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+    }
+    public static void line(){
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println();
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
+
---
+++ src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter <6; counter++){
+            star();
+        }
+    }
+    public static void star(){
+        for (int x = 0; x <10; x++){
+            System.out.print(x+ " ");
+        }
+        System.out.println();
+    }
+}
