7b1dccf928879191eb207fa9a43d7ff4601a4efd
Dwalker1000 <25walkerd@gmail.com>
2020-09-14 20:21:06 -0500

    all files

--- src/TemplateOne.java
+++ src/TemplateOne.java
@@ -1,5 +1,7 @@
+//In Java a program is called a class
 public class TemplateOne {
-
+//In Java functions are called methods
+    //main method is the one that runs your program
     public static void main(String[] args) {
 
         System.out.println("hello world");
