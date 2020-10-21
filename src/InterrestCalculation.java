fbc89263058261abf08d37f9c05dd370217c336d
Dwalker1000 <25walkerd@gmail.com>
2020-10-21 09:29:37 -0500

    update

--- src/InterrestCalculation.java
+++ src/InterrestCalculation.java
@@ -7,20 +7,21 @@ public class InterrestCalculation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("whats your initial investment");
-        double input = scan.nextDouble();
+        float input = scan.nextFloat();
         System.out.println("how many years do you want this to go on for");
         int years = scan.nextInt();
         System.out.println("whats your intrest rate (dont put it in persent form ex: 5,6.5, 2.3)");
-        double intrestRate = scan.nextDouble();
+        float intrestRate = scan.nextFloat();
         System.out.println("how many dollars do you want to invest per year");
-        double invesymentPerYear = scan.nextDouble();
+        float invesymentPerYear = scan.nextFloat();
         for (int x = 1; x <= years; x++) {
-            input = input + rateclaculator(input, intrestRate) + invesymentPerYear;
-            System.out.println("after "+ x + " years you will have $" + (float)input + " in your savings");
+            input = input + rateclaculator(input, intrestRate);
+            System.out.println("in "+ x+" years you made $"+input);
+            input = input + invesymentPerYear;
         }
     }
 
-    public static double rateclaculator(double amount, double intRate) {
+    public static float rateclaculator(float amount, float intRate) {
         amount = amount * intRate / 100;
         return amount;
     }
