09a6e02eb03f2ddbf72a68ab40a42966df406310
Dwalker1000 <25walkerd@gmail.com>
2020-11-06 09:04:37 -0600

    small update

--- src/point.java
+++ src/point.java
@@ -5,6 +5,7 @@ public class point {
     //fields
     private int x = 3;
     private int y = 7;
+    private String name;
     //create cunstructors
     public point(){
         x=-1;
@@ -13,15 +14,16 @@ public class point {
     public point(int x1, int y1){
         x=Math.abs(x1);
         y=Math.abs(y1);
+        name = ("this is point "+ x+" +"+ y);
     }
-    //non static method changing the feild values Mutator methods
+    //non static method changing the field values Mutator methods
     public void SetX(int x1){
         x=x1;
     }
     public void SetY(int y1){
         y =y1;
     }
-    //non static method object refrence has to be made
+    //non static method object reference has to be made
     //return values Assesor methods
     public int GetX(){
         return x;
