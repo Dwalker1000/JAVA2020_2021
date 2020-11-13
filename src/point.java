5419aa7ef321edd3939d11b950b333ceef6d54b8
Dwalker1000 <25walkerd@gmail.com>
2020-11-13 08:55:37 -0600

    fix in point.java

--- src/point.java
+++ src/point.java
@@ -3,38 +3,41 @@
 public class point {
     public int x;
     public int y;
-    // constructs a new point at the origin, (0, 0)
+    private String name;
     public point() {
-        this(0, 0); // calls Point(int, int) constructor
+        x=0;
+        y=0;
+        name = "this is the x" + x + "this is the y" + y;
     }
-    // constructs a new point with the given (x, y) location
-    public point(int x, int y) {
-        setLocation(x, y);
-    }
-// returns the distance between this Point and (0, 0)
-    public double distanceFromOrigin() {
-        return Math.sqrt(x * x + y * y);
+    public point(int x1, int y1) {
+        if (x1 < 0 || y1 < 0){
+            throw new IllegalArgumentException("can be less that zero");
         }
-    // returns the x-coordinate of this point
+        else{
+            x=x1;
+            y=y1;
+            name = "this is the x" + x + "this is the y" + y;
+        }
+        setLocation(x1, y1);
+    }
+    public double distanceFromOrigin() {
+        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+        }
     public int getX() {
         return x;
     }
-    // returns the y-coordinate of this point
     public int getY() {
     return y;
     }
-    // sets this point's (x, y) location to the given values
     public void setLocation(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    // returns a String representation of this point
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
-    // shifts this point's location by the given amount
-    public void translate(int dx, int dy) {
-        setLocation(x + dx, y + dy);
+    public void translate(int x2, int y2) {
+        setLocation(x + x2, y + y2);
     }
     //hw
     public int manhattanDistance(point other){
