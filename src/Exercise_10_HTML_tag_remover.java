200b06bea946bc123c7e60bd545ffc6ae97b00ed
Dwalker1000 <25walkerd@gmail.com>
2020-12-02 10:05:09 -0600

    update files

--- src/Exercise_10_HTML_tag_remover.java
+++ src/Exercise_10_HTML_tag_remover.java
@@ -3,13 +3,15 @@ import java.util.*;
 public class Exercise_10_HTML_tag_remover {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
+        //creates new file
+        PrintStream newFile = new PrintStream("C:\\Users\\walkerd24\\IdeaProjects\\JAVA2020_2021\\src\\NewRandomeFile.txt");
         System.out.println("whats the full pah to your file");
         String name = in.nextLine().replace("\\", "\\\\");
         Scanner scan = new Scanner(new File(name));
-        stripHtmlTags(scan);
+        stripHtmlTags(scan, newFile);
     }
 
-    public static void stripHtmlTags(Scanner input) {
+    public static void stripHtmlTags(Scanner input, PrintStream newFile) {
         while (input.hasNextLine()) {
             String line = input.nextLine();
 
@@ -22,8 +24,8 @@ public class Exercise_10_HTML_tag_remover {
                     line = line.substring(0, index1) + line.substring(index2 + 1);
                 }
             }
-
-            System.out.println(line);
+            //adds to the new file
+            newFile.println(line);
         }
     }
 }
