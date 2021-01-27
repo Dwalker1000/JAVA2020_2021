d94fecc3e928c32993b2dd9db6d9841e9cb8b027
Dwalker1000 <25walkerd@gmail.com>
2021-01-26 20:08:10 -0600

    needs more research

---
+++ src/autoClicker.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+public class autoClicker {
+    final int speed = 10;
+    final int stopKey = 1;
+    public static void main (String [] args) throws AWTException {
+        Robot robot = new Robot();
+        int x = 1;
+        while (x == 1) {
+            if ();
+            robot.mousePress(InputEvent.BUTTON1_MASK);
+            Thread.sleep(speed);
+
+        }
+    }
+}
